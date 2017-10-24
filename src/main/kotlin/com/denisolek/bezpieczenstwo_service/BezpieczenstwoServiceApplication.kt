9614147f86172795415ca6a9e300e757313de4e2
denisolek <denis.olek@gmail.com>
2017-10-24 14:18:38 +0200

    init gradle setup with docker

---
+++ src/main/kotlin/com/denisolek/bezpieczenstwo_service/BezpieczenstwoServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.denisolek.bezpieczenstwo_service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BezpieczenstwoServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BezpieczenstwoServiceApplication::class.java, *args)
+}
