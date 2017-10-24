28895d16a65deb3ce3443a7f47a2da59f543b84e
denisolek <denis.olek@gmail.com>
2017-10-24 14:42:57 +0200

    CORS config

---
+++ src/main/kotlin/com/denisolek/bezpieczenstwo_service/config/CORSConfig.kt
@@ -0,0 +1,25 @@
+package com.denisolek.bezpieczenstwo_service.config
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CORSConfig : CorsFilter(configurationSource()) {
+    companion object {
+        fun configurationSource(): UrlBasedCorsConfigurationSource {
+            val config = CorsConfiguration()
+            config.allowCredentials = true
+            config.addAllowedOrigin("*")
+            config.addAllowedHeader("*")
+            config.addAllowedMethod("*")
+            val source = UrlBasedCorsConfigurationSource()
+            source.registerCorsConfiguration("/**", config)
+            return source
+        }
+    }
+}
